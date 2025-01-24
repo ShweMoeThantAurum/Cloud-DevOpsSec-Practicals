bb7ca7e5355ed7954fae3086e33e8ce6f54dfa73
ShweMoeThantAurum <shwemoethant04@gmail.com>
2025-01-24 21:05:45 +0000

    Rename the file

--- Week01/Problem01/App01.py
+++ Week01/Problem01/App01.py
@@ -1,5 +1,5 @@
 # Question 1 - Develop an application that asks the user for her/his name, and then greets them with their name.
 
-from Problem01 import Greeting
+from Program01 import Greeting
 username = input("Enter your username: ")
 print(Greeting.do_greeting(username))
